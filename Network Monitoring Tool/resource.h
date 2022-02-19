17f0ebd9f467dcae79d27ba8682430e35e7ab624
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2022-02-19 21:12:36 +0800

    added Icon to Tool

--- Network Monitoring Tool/resource.h
+++ Network Monitoring Tool/resource.h
@@ -7,7 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_CHECKOPENPORST_DIALOG       102
 #define IDR_MAINFRAME                   128
-#define IDB_BITMAP1                     130
+#define IDB_BITMAP1                     135
 #define IDC_IPADDRESS_IP                1000
 #define IDC_EDIT_AREA                   1001
 #define IDC_BUTTON_PORT                 1002
@@ -41,7 +41,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1024
 #define _APS_NEXT_SYMED_VALUE           101
